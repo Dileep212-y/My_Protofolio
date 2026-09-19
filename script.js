3c5a51bc70b8c2e11a9f10e08e8603a7b77aa6ec
Dileep Boddu <dileepboddu0685@gmail.com>
2026-09-19 12:17:30 +0530

    Animate actual project screenshots through scroll

--- script.js
+++ script.js
@@ -17,11 +17,19 @@ gsap.to(".w3",{x:-100,scrollTrigger:{trigger:".statement",start:"top bottom",end
 
 const projects=gsap.utils.toArray(".project");
 projects.forEach((p,i)=>{
- const copy=p.querySelector(".project-copy"),screen=p.querySelector(".screen");
+ const copy=p.querySelector(".project-copy"),screen=p.querySelector(".screen"),shots=p.querySelectorAll(".shot");
  gsap.from(copy,{x:-120,opacity:0,scrollTrigger:{trigger:p,start:"top 75%",end:"top 30%",scrub:1}});
  gsap.from(screen,{x:260,rotationY:-24,rotationZ:i%2?2:-3,scale:.72,opacity:0,scrollTrigger:{trigger:p,start:"top 85%",end:"center center",scrub:1}});
  gsap.to(screen,{y:-70,rotationY:i%2?8:-2,scale:1.04,scrollTrigger:{trigger:p,start:"center center",end:"bottom top",scrub:1}});
  gsap.to(copy,{y:-80,scrollTrigger:{trigger:p,start:"center center",end:"bottom top",scrub:1}});
+ if(shots.length){
+   const tl=gsap.timeline({scrollTrigger:{trigger:p,start:"top top",end:"bottom bottom",scrub:1}});
+   shots.forEach((shot,j)=>{
+     const enter=j===0?0:j*0.23;
+     tl.to(shot,{opacity:1,x:0,y:0,scale:1,duration:.18,ease:"power2.out"},enter)
+       .to(shot,{opacity:j===shots.length-1?1:0,x:j%2? -55:55,y:j%2?25:-25,scale:.94,duration:.16,ease:"power2.in"},enter+.16);
+   });
+ }
 });
 gsap.from(".about-wrap",{y:100,opacity:0,scrollTrigger:{trigger:".about",start:"top 65%",end:"center center",scrub:1}});
 gsap.from(".stack-cloud span",{y:80,opacity:0,stagger:.04,scrollTrigger:{trigger:".stack",start:"top 70%",end:"center center",scrub:1}});
